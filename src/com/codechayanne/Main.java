364c57190e6c2b1b9bf33043b0c3a62fceff0479
Chayanne Paulino <chayanne@Chayannes-MacBook-Pro.local>
2020-06-16 22:53:51 -0400

    Program run class

--- src/com/codechayanne/Main.java
+++ src/com/codechayanne/Main.java
@@ -1,8 +1,41 @@
 package com.codechayanne;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main extends EmailAdm{
 
     public static void main(String[] args) {
-	// write your code here
+        /* Input Object */
+
+        Scanner in = new Scanner(System.in);
+
+        EmailAdm userEm = new EmailAdm();
+
+
+        System.out.print("Please enter the employee full name: ");
+
+        userEm.setFirstname(in.nextLine());
+        userEm.setLastname(in.nextLine());
+
+        System.out.print("Select department: ");
+
+        int department = in.nextInt();
+
+        userEm.setDepartment(department);
+
+        System.out.println(" ");
+        System.out.println("This is your Employee email account Information: ");
+        System.out.println(" ");
+        System.out.println("Email Address: " + userEm.email());
+        System.out.println(" ");
+        System.out.println("Department: " + userEm.getDepartment());
+        System.out.println(" ");
+        System.out.println("Email Capacity: "+ userEm.getEmailcapacity());
+        System.out.println(" ");
+        System.out.print("Password : ");
+        userEm.password();
+
+
+
     }
 }
